6258f0dbefede811143931746df64283ae9340ce
priyadarshii786 <tathagatp918@gmail.com>
2023-10-20 00:59:26 +0530

    closer

---
+++ src/components/basics/menuApi.js
@@ -0,0 +1,119 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 10,
+        image: "../images/Bhelpuri.jpg",
+        name: "Bhelpuri",
+        category: "evening",
+        price: "15₹",
+        description:" A popular street food snack made with puffed rice, sev, peanuts, onions, tomatoes, and tamarind chutney. It has a crunchy texture and a sweet, tangy, and spicy flavor."
+    },
+    {
+        id: 11,
+        image: "../images/alootikki.jpg",
+        name: "Aloo Tikki",
+        category: "evening",
+        price: "20₹",
+        description:" A shallow-fried potato patty that is crispy on the outside and soft on the inside, mixed with peas and spices. It is usually served with mint chutney and tamarind chutney."
+    },
+    {
+        id: 12,
+        image: "../images/Pani-Puri.jpg",
+        name: "Pani Puri",
+        category: "evening",
+        price: "20₹",
+        description:" A popular street food snack made with crispy hollow balls filled with a spiced mixture of chickpeas, potatoes, and tamarind water. It has a sweet, tangy, and spicy flavor."
+
+    },
+    {
+        id: 13,
+        image: "../images/masala-vada.png",
+        name: "Masala Vada ",
+        category: "evening",
+        price: "20₹",
+        description:"A spicy and crunchy South Indian snack made with chana dal, onion, and spices. It is usually served with coconut chutney and sambar."
+    }
+  ];
+  
+  export default Menu;
